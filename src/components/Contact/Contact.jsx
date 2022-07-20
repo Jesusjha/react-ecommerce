28d6a9ad79e876a31c6199693088954dfb2c69e0
Jesus Herrera <heisusin@gmail.com>
2022-07-20 02:20:36 +0200

    feat: :sparkles: Created 'Contact Us' section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import { Card, Container, Text, Input, Textarea, Button } from '@nextui-org/react';
+import React from 'react';
 
 function Contact() {
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<Container>
+				<Card css={{ bc: '$accents9', w: '80rem', margin: '2rem auto' }}>
+					<Card.Body css={{ margin: 'auto' }}>
+						<Text color='#fff' h2>
+							Contact Us
+						</Text>
+					</Card.Body>
+				</Card>
+			</Container>
+			<Container
+				css={{
+					display: 'flex',
+					justifyContent: 'center',
+					flexDirection: 'column',
+					w: '30rem',
+				}}
+			>
+				<Input bordered label='Your Name' type='text'></Input>
+				<Input bordered css={{ mt: '1.5rem' }} label='Your Email' type='text'></Input>
+				<Input bordered css={{ mt: '1.5rem' }} label='Your Phone' type='text'></Input>
+				<Textarea
+          bordered
+          rows={8}
+          css={{ mt: '1.5rem', }}
+					label='Your Message'
+				/>
+        <Button color='error' css={{mt:'2rem'}}>Send Message</Button>
+
+			</Container>
+		</>
+	);
 }
 
-export default Contact
+export default Contact;
